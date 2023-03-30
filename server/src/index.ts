f5f903cad063fcdd8e7b46cd024ad0307f41bba7
Leonardo Motta <leonardomotta@ucl.br>
2023-03-30 14:44:19 -0300

    add socket.io on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello world!');
+const  express = require('express');
+const app = require('express')();
+app.use(express.static(__dirname + '/../public'))
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
+
+const clients : Array<any>  = [];
+
+io.on('connection', (client) => {
+    console.log(`Client connected ${client.id}`);
+    clients.push(client);
+
+    client.on('disconnect', () => {
+        clients.splice(clients.indexOf(client), 1);
+        console.log(`Client disconnected ${client.id}`);
+    });
+});
+
+app.get('/msg', (req, res) => {
+    const msg = req.query.msg || '';
+    for(const client of clients) {
+        client.emit('msg', msg);
+    }
+
+    res.json({
+        ok: true
+    });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
