64230fc833c9361a0b2185cff1f4648b7183f0fd
Leonardo Motta <leonardomotta@ucl.br>
2023-03-31 12:07:48 -0300

    mqtt connection test

--- client/src/pages/Tracker/index.tsx
+++ client/src/pages/Tracker/index.tsx
@@ -6,16 +6,25 @@ const socket = io('http://localhost:5000', {
     transports: ['websocket']
 });
 
+
+
 export default function Tracker() {
+    const [socketData, setSocketData] = useState<null | any>(null);
+    const [object,SetObject] = useState<any>({});
     
     useEffect(() => {
         socket.connect();
-        socket.emit("reply");
+        socket.on('mqttData', (JSONdata)=>{
+            setSocketData(JSON.stringify(JSONdata));
+            SetObject(JSONdata);
+        });
     }, []);
 
     return (
         <div>
-            <h1>Olá Mundo</h1>
+            <h1>Web Socket Client</h1>
+            <h2>Data: {socketData ? socketData : 'No data available!'}</h2>
+            <h2>msg: {object.msg ? object.msg : 'No object!'}</h2>
         </div>
     )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,10 +3,10 @@ const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const cors = require('cors');
+const mqtt = require('mqtt')
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.static(__dirname + '/../public'))
 
 const clients : Array<any>  = [];
 
@@ -18,22 +18,45 @@ io.on('connection', (client) => {
         clients.splice(clients.indexOf(client), 1);
         console.log(`Client disconnected ${client.id}`);
     });
-
-    //TESTE
-    client.on('reply', () => { console.log("a reply detected!")});
 });
 
-app.get('/msg', (req, res) => {
-    const msg = req.query.msg || '';
-    for(const client of clients) {
-        client.emit('msg', msg);
+//MQTT
+const mqtt_host = 'broker.emqx.io'
+const mqtt_port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${mqtt_host}:${mqtt_port}`
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'emqx',
+  password: 'public',
+  reconnectPeriod: 1000,
+})
+
+const topic = '/nodejs/mqtt'
+client.on('connect', () => {
+  console.log('MQTT Connected')
+
+  client.subscribe([topic], () => {
+    console.log(`Subscribed to topic '${topic}'`)
+  })
+})
+
+client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+
+    try {
+        const payloadObject = JSON.parse(payload);
+        console.log(payloadObject);
+        io.emit('mqttData',payloadObject);
+    } catch (err) {
+        console.error(`Error parsing JSON: ${err}`);
     }
+})
 
-    res.json({
-        ok: true
-    });
-});
-
+// Server
 server.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
